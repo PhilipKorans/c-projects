f8a91c663ddb3dd1e4306110a35afeb6f7405c7c
PhilipKorans <06koranteng@gmail.com>
2022-08-30 13:33:01 +0000

    Standard I/O

---
+++ io_demo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char f,m,l;
+	int age;
+
+	scanf("%c %c %c %d, &f, &m, &l, &age);
+	printf("My initials are: %c%c%c and my age is %d.\n", 'P','O','K', 19);
+	return (0);
+}
